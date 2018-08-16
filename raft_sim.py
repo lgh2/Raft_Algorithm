93f4b2cfe58c37d9e8699cd5573f7600d76d6ba9
Laura Hampton <lhampton@gmail.com>
2018-08-16 12:01:03 -0400

    debugging client __call__

--- raft_sim.py
+++ raft_sim.py
@@ -130,7 +130,6 @@ class RaftNode:
             vote=None,
             leader_id = None
             ):
-        #creates single messages
         try:
             self.new_log_entry = self.log[-1]
         except IndexError:
@@ -196,7 +195,7 @@ class RaftNode:
                 logger.debug("received response from {}".format(message.sender_id))
         else:
             pass
-            #reject entry
+            #TODO: reject entry
 
     def commit_data(self,data):
         try:
@@ -246,11 +245,6 @@ class RaftNode:
             #send msg
 
 
-    #if not voted this term, have not already voted for server, vote
-    # else deny vote
-    # voting as bool vs check term number
-
-
     def show_logs(self):
         print ("status: ", self.status)
         print("log: ", self.log)
@@ -266,16 +260,14 @@ class RaftClient:
         self.leader = None
         self.config = config
         self.q = q
-        # print(self.id)
 
     async def __call__(self):
-        while True:
-            for recipient in self.config:
-                message = self.create_message(recipient)
-                await self.q.put(message)
-                logger.debug("{} put {}".format(self.id, type(message)))
-            await self.q.task_done()
-            await curio.sleep(7)
+        for recipient in self.config:
+            message = self.create_message(recipient)
+            await self.q.put(message)
+            logger.debug("{} put {}".format(self.id, type(message)))
+        await self.q.task_done()
+
 
     async def recieve(self, message):
         if self.id not in message.recipient_id:
@@ -304,7 +296,6 @@ class RaftClient:
         print("Client log: ", self.log)
 
 async def main():
-    #check this against prod_cons.py
     config = ["Alice", "Bob"]
     q = curio.Queue()
     alice = RaftNode("Alice", config, q)
@@ -322,97 +313,3 @@ async def main():
 
 if __name__=='__main__':
     curio.run(main, with_monitor=True)
-
-
-"""
-#--------------------------------------------------------------
-
-# cf error messages in raft.go
-
-# only 2 forms of communication for basic algo - request vote and append entries - servers retry requests if they do not recieve timely replies
-# Heartbeats are append entries requests with no data
-# observer object
-# client object
-# logging
-
-# match index - index of highest known match on leader
-
-# find peers - adding/removing peers can only be run on the leader (must fail otherwise); if leader is removed it triggers a new election
-# channel
-#leaders inform peers when elected and when they become followers (cf split election)
-# followers track last time of contact from a leader
-
-# if no heartbeat/haven't heard from a leader in time, revert to candidate state
-# when leader steps down - cf cleanup steps in go - reset last contact time
-# if a client sends data to a follower, follower redirects request to last leader they heard from
-
-# all communications between servers have term numbers included
-# if a server recieves a message with a larger term number, it updates its own term number
-#if a leader or candidate recieves a message with a larger term number it
-reverts to a follower
-# servers reject requests with stale term numbers (smaller than current)
-
-#requests for votes are sent in parallel (as are heartbeats/add log entries)
-#data cannot be sent during election (no leader)
-
-#VOTING
-# new leader is chosen when existing leader fails - leaders typically operate until they fail
-# if a follower has not heard from a leader for the election timeout, it
-# increments its term, becomes a candidate and initiates an election
-# if awaiting votes, a candidate recieves an append entries message from a server with a term as high as or higher than its own, it reverts to follower status
-# if term number is lower than its own, rejects append entries message and continues election
-# Raft determines which of two logs is more up-to-date
-# by comparing the index and term of the last entries in the
-# logs. If the logs have last entries with different terms, then
-# the log with the later term is more up-to-date. If the logs
-# end with the same term, then whichever log is longer is
-# more up-to-date.
-#if a server recieves a request vote message from another server but the minimum election timeout for the current leader has not passed since the last heartbeat, it does not change its term or send its vote
-
-#LOGGING
-# leader sends commit logs message when a majority of the servers have acknowledged they have added the data to their logs
-# logs are applied in order
-# When sending # an AppendEntries RPC, the leader includes the index
-# and term of the entry in its log that immediately precedes
-# the new entries. If the follower does not find an entry in
-# its log with the same index and term, then it refuses the
-# new entries
-# logs only flow from leader to follower (leaders never delete logs)
-
-# To bring a follower’s log into consistency with its own,
-# the leader must find the latest log entry where the two
-# logs agree, delete any entries in the follower’s log after
-# that point, and send the follower all of the leader’s entries
-# after that point.
-
-# The leader maintains a nextIndex for each follower,
-# which is the index of the next log entry the leader will
-# send to that follower. When a leader first comes to power,
-# it initializes all nextIndex values to the index just after the
-# last one in its log (11 in Figure 7). If a follower’s log is
-# inconsistent with the leader’s, the AppendEntries consistency
-# check will fail in the next AppendEntries RPC. After
-# a rejection, the leader decrements nextIndex and retries
-# the AppendEntries RPC. Eventually nextIndex will reach
-# a point where the leader and follower logs match. When
-# this happens, AppendEntries will succeed, which removes
-# any conflicting entries in the follower’s log
-
-# leader never changes its log if follower's log is inconsistent
-#leaders commit a no-op message at the start of their terms to confirm that followers have the latest logs committed
-# leaders must exchange heartbeats with majority of servers to confirm they are still leader before responding to a read request
-
-# // Stats is used to return a map of various internal stats. This
-# // should only be used for informative purposes or debugging.
-# //
-# // Keys are: "state", "term", "last_log_index", "last_log_term",
-# // "commit_index", "applied_index", "fsm_pending",
-# // "last_snapshot_index", "last_snapshot_term", "num_peers" and
-# // "last_contact".
-
-#client - chooses random server on startup, if server is not leader, it provides network address of last leader it heard from
-# if no leader, client retries
-#client commands have unique serial numbers
-# if serial no is not confirmed, client retries
-# if client re-sends a committed serial number, leader can confirm without contacting other servers
-"""
